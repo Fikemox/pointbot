87e9633d1b44ef938620dfdce296a5c7e0436853
fikemox <mjfox09@gmail.com>
2018-08-06 21:42:05 -0400

    Add first pass pointChange and me postgres db statements

--- src/commands/me.ts
+++ src/commands/me.ts
@@ -1,9 +1,18 @@
 import { Message } from "discord.js";
 import { Client } from "pg";
 
-export function me(dbClient: Client, message: Message, args: string[], command: string) {
+export async function me(dbClient: Client, message: Message, args: string[], command: string) {
     // Delete the message from the channel
     message.delete().catch(O_o=>{console.log(O_o);});
+    let member = message.author;
+    await dbClient.connect();
+
+    let pointResult = await dbClient.query(`SELECT point_value FROM UserPoints WHERE user_id = ${member.id}`);
+    let currentPoints = 0;
+    if (pointResult.rowCount > 0) {
+        currentPoints = pointResult.rows[0].point_value;
+    }
+    message.reply(`Your life is currently worth: ${currentPoints}\n <Insert witty ePeen joke here>`);
     //Todo: Fill in with real list
-    message.reply("This is where your points would go...**IF I HAD THEM!**", {files: ["https://memegenerator.net/img/images/2276176.jpg"]});
+    //message.reply("This is where your points would go...**IF I HAD THEM!**", {files: ["https://memegenerator.net/img/images/2276176.jpg"]});
 }
--- src/commands/pointChange.ts
+++ src/commands/pointChange.ts
@@ -2,7 +2,7 @@ import { Message } from "discord.js";
 import { Commands } from "./index";
 import { Client } from "pg";
 
-export function pointChange(dbClient: Client, message: Message, args: string[], command: string) {
+export async function pointChange(dbClient: Client, message: Message, args: string[], command: string) {
     // Delete the message from the channel
     message.delete().catch(O_o=>{console.log(O_o);});
 
@@ -19,10 +19,31 @@ export function pointChange(dbClient: Client, message: Message, args: string[], 
         reason = "Reasons!";
     }
 
+    // Connect to the database
+    await dbClient.connect();
+    // Get the member's current point value so we can modify it
+    let pointResult = await dbClient.query(`SELECT point_value FROM UserPoints WHERE user_id = ${member.id}`);
+    let currentPoints = 0,
+        insertIntoTable = true;
+    if (pointResult.rowCount > 0) {
+        currentPoints = pointResult.rows[0].point_value;
+        insertIntoTable = false;
+    }
+
     // Add the point
     if (command === Commands.addPoint) {
         message.reply(`${member.user.tag} has been awarded a point by ${message.author.tag} because: ${reason}`);
+        currentPoints++;
     } else if (command === Commands.subPoint) {
         message.reply(`${member.user.tag} has been deducted a point by ${message.author.tag} because: ${reason}`);
+        currentPoints--;
     }
+
+    // Update the member's current point value
+    if (insertIntoTable) {
+        await dbClient.query(`INSERT INTO UserPoints(user_id, point_value) VALUES (${member.id}, ${currentPoints}`);
+    } else {
+        await dbClient.query(`UPDATE UserPoints SET point_value = ${currentPoints} WHERE user_id = ${member.id}`);
+    }
+    await dbClient.end();
 }
