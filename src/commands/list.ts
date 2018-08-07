2d0be67a6c9c2925abdf3ed5437ff0eea202239f
fikemox <mjfox09@gmail.com>
2018-08-06 23:20:52 -0400

    list mods to make a better list

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -4,24 +4,24 @@ import * as Discord from "discord.js";
 
 export async function list(dbClient: Client, message: Message, args: string[], command: string) {
     try {
+        // Grab the list data
         const listResult = await dbClient.query(`SELECT * FROM UserPoints;`);
         if (listResult.rowCount <= 0) {
             message.channel.send("This is where the list would go...**IF I HAD ONE!**", {files: ["https://memegenerator.net/img/images/2276176.jpg"]});
             return;
         }
-        console.log(listResult.rows);
+        // Start building the list
         let botresponse = new Discord.RichEmbed()
 			  .setTitle("Leaderboard")
 			  .setDescription("All the points!")
 			  .setColor(0x00AE86);
         for(let i = 0; i < listResult.rowCount; i++) {
-            const member = message.guild.members.get(listResult.rows[i].user_id);
-            // @ts-ignore
+            const member = (message.guild.members.get(listResult.rows[i].user_id) as Discord.GuildMember);
             botresponse.addField(`${member.user.username} :`,` ${listResult.rows[i].point_value}`);
         }
+        // Send the response into the server
         message.channel.send(botresponse);
     } catch (error) {
         console.log("An error occurred running the !list command: " + error.message);
     }
-    //Todo: Fill in with real list
 }
